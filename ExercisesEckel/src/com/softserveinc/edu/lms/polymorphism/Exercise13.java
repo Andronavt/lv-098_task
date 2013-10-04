d1c10db46037a6411519814d94f3e3137831d8fe
Maryan.git <maryan36786@gmail.com>
2013-10-04 14:08:20 +0300

    Exercise13

---
+++ ExercisesEckel/src/com/softserveinc/edu/lms/polymorphism/Exercise13.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.softserveinc.edu.lms.polymorphism;
+
+/**
+ * @author Maryan
+ *
+ */
+public class Exercise13 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}

afe5f4ab33b0fb26d02e77f8aa00995435cf4293
Orest <orecto@gmail.com>
2013-10-04 14:25:42 +0300

    Add 3

--- Ekkel/src/holdingYourObjects/Exercise26.java
+++ Ekkel/src/holdingYourObjects/Exercise26.java
@@ -5,11 +5,13 @@ package holdingYourObjects;
 
 /**
  * @author orecto
- *
+ * 
  */
 public class Exercise26 {
 	public static void main(String[] args) {
-System.out.println("sdasd");
+		System.out.println("sdasd");
+		System.out.println("sdasd");
+		System.out.println("sdasd");
 	}
 
 }

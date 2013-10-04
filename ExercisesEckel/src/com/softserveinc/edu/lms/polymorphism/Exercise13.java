acd7a4b291738d3b3b364fc63d935e1cb1db63a9
Maryan.git <maryan36786@gmail.com>
2013-10-04 18:16:16 +0300

    chek commit Exercise13

--- ExercisesEckel/src/com/softserveinc/edu/lms/polymorphism/Exercise13.java
+++ ExercisesEckel/src/com/softserveinc/edu/lms/polymorphism/Exercise13.java
@@ -5,16 +5,67 @@ package com.softserveinc.edu.lms.polymorphism;
 
 /**
  * @author Maryan
- *
+ * 
  */
-public class Exercise13 {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
+class Shared {
+    private int refcount = 0;
+    private static long counter = 0;
+    private final long id = counter++;
 
+    public Shared() {
+	System.out.println("Creating " + this);
     }
+
+    public void addRef() {
+	refcount++;
+    }
+
+    protected void dispose() {
+	if (--refcount == 0) {
+	    System.out.println("Disposing " + this);
+	}
+    }
+
+    public String toString() {
+	return "Shared " + id;
+    }
+}
+
+class Composing {
+    private Shared shared;
+    private static long counter = 0;
+    private final long id = counter++;
+
+    public Composing(Shared shared) {
+	System.out.println("Creating " + this);
+	this.shared = shared;
+	this.shared.addRef();
+    }
+    
+    protected void dispose() {
+	System.out.println("disposing " + this);
+	shared.dispose();
+    }
+    
+    public String toString() {
+	return "Composing " + id;
+    }
+}
+
+class ReferenseCounting {
+    public static void main(String[] args) {
+	Shared shared = new Shared();
+	Composing[] composing = { new Composing(shared), new Composing(shared),
+		new Composing(shared), new Composing(shared), new Composing(shared)
+	};
+	
+	for (Composing c : composing) {
+	    c.dispose();
+	}
+    }
+}
+
+public class Exercise13 extends ReferenseCounting {
 
 }

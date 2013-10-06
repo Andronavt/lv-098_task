dfe43f039b32f5cbe80fe7c57f5d22469743653a
Orest <orecto@gmail.com>
2013-10-06 20:33:23 +0300

    +25

--- Ekkel/src/holdingYourObjects/Exercise25.java
+++ Ekkel/src/holdingYourObjects/Exercise25.java
@@ -3,6 +3,14 @@
  */
 package holdingYourObjects;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import staticPack.TextFile;
+
 /**
  * @author orecto
  * 
@@ -17,6 +25,23 @@ package holdingYourObjects;
  */
 public class Exercise25 {
 	public static void main(String[] args) {
+		Map<String, ArrayList<Integer>> map = new TreeMap<String, ArrayList<Integer>>();
+		List<String> listOfWords = new ArrayList<String>(new TextFile(
+				"Superpuper.txt", "\\W+"));
+		System.out.println("We read next words: " + listOfWords);
+		Iterator<String> it = listOfWords.iterator();
+		int count = 0;
 
+		while (it.hasNext()) {
+			String s = it.next();
+			count++;
+			if (!map.keySet().contains(s)) {
+				ArrayList<Integer> list = new ArrayList<Integer>();
+				list.add(count);
+				map.put(s, list);
+			} else 
+				map.get(s).add(count);
+		}
+		System.out.println("Location of words in the file: " + map);
 	}
 }

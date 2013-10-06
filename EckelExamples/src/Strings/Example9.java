3a20986eb2f6eb260acac1a4743ed03fe49c0a7b
Oleg Yatskiv <olegyatskiv@gmail.com>
2013-10-06 22:31:50 +0300

    Example9

---
+++ EckelExamples/src/Strings/Example9.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Strings;
+
+/**
+ * @author Oleg
+ * 
+ */
+public class Example9 {
+
+	public static String knights = "Then, when you have found the shrubbery, you must "
+			+ "cut down the mightiest tree in the forest... "
+			+ "with... a herring!";
+	public static void main(String[] args) {
+		System.out.println(knights.replaceAll("[aeiouAEIOU]", "+"));
+	}
+}

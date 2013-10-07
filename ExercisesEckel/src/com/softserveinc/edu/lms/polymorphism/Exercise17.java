e9bdac02292030bd77687a7d8c39fdb7d138d24c
Maryan.git <maryan36786@gmail.com>
2013-10-07 19:03:20 +0300

    Exercise17-2

--- ExercisesEckel/src/com/softserveinc/edu/lms/polymorphism/Exercise17.java
+++ ExercisesEckel/src/com/softserveinc/edu/lms/polymorphism/Exercise17.java
@@ -34,35 +34,19 @@ class Tricycle1 extends Cycle1 {
 }
 
 public class Exercise17 extends Cycle1{
-    private static Cycle1[] arr = new Cycle1[3];
+    private static Cycle1[] arr = {new Unicycle1(), new Bicycle1(), new Tricycle1()};
     /**
      * @param args
      */
     
     public static void main(String[] args) {
-	
-	Cycle1 c1 = initUnicycle1();
-	Cycle1 c2 = initBicycle1();
-	Cycle1 c3 = initTricycle1();
-	
-	
+	/*for (Cycle1 c: arr) {
+	    c.balance(); // Неможливо викликати мето balance();
+	} */
+
 	Unicycle1 u = new Unicycle1();
 	u.balance();
 	Bicycle1 b = new Bicycle1();
 	b.balance();
-	
-	
-    }
-    
-    public static Cycle1 initUnicycle1() {
-	return arr[0] = new Unicycle1();
-    }
-
-    public static Cycle1 initBicycle1() {
-	return arr[1] = new Bicycle1();
-    }
-    
-    public static Cycle1 initTricycle1() {
-	return arr[2] = new Tricycle1();
     }
 }

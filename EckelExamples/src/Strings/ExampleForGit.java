732496e03062d037ae8370803a79d8a09e8c0956
Oleg Yatskiv <olegyatskiv@gmail.com>
2013-10-08 12:35:35 +0300

    MyExample

---
+++ EckelExamples/src/Strings/ExampleForGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Strings;
+
+/**
+ * @author Oleg
+ *
+ */
+public class ExampleForGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("dafasd");
+	}
+
+}
